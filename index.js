6a2a8e55cc00ed72b1b84e1f8d29ae0cc318463b
PabloPicas0 <pawel.fe@gmail.com>
2023-03-15 23:14:16 +0100

    correct ip response

--- index.js
+++ index.js
@@ -6,18 +6,19 @@ const app = express();
 
 app.use(cors({ optionsSuccessStatus: 200 }));
 app.use(express.static("public"));
+app.set("trust proxy", true);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
 app.get("/api/whoami", (req, res) => {
-  const ip = req.socket.remoteAddress
+  const ip = req.ip;
   const language = req.header("accept-language");
   const software = req.header("user-agent");
 
   res.json({ ipaddress: ip, language: language, software: software });
-})
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log(`Your app is hosted on localhost:${listener.address().port}`);
